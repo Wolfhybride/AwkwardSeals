b711625fb879d3fc630b0c7d0d0c26fda4fed5d7
knetterzak <RoxanneVdPol@gmail.com>
2016-11-09 02:15:57 +0100

    Create main.py
    de main file met de gegevens voor het laden van de I/O en het scherm.

---
+++ Game/main.py
@@ -0,0 +1,43 @@
+from tkinter import *
+import xmltodict
+import time
+
+with open("file.xml") as fd:
+    doc = xmltodict.parse(fd.read())
+
+root = Tk()
+root.title("Student Life")
+root.resizable(width=False, height=False)
+
+mainWindow = Text(
+    root,
+    height=30,
+    width=100,
+    background = ('black'),
+    foreground = ('white'),
+    font = ('Courier',12),
+    cursor = 'plus',
+    state = 'disable'
+)
+mainWindow.pack(anchor = 'center')
+
+def write(string):                                          #print stuff to screen as text
+    mainWindow.config(state='normal')
+    mainWindow.insert("end",string + "\n")
+    mainWindow.see("end")
+    mainWindow.config(state='disable')
+
+
+console = Entry(root,
+                width=100,
+                background='#3D3D3D',
+                foreground='white',
+                #font=('Courier', 10),
+                relief='flat',
+                borderwidth=0,
+                )
+console.pack(anchor = 'center',
+             #fill = 'both',
+             side = 'bottom'
+             )
+#console.insert(0,">>> ")
